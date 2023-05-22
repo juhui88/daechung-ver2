1c48fe34c33ca7bc7d37dd44619246a43a5315d1
juhui88 <sand8594@hufs.ac.kr>
2023-05-22 21:35:04 +0900

    Update homePage

--- daechung-ver2/components/Calendar.js
+++ daechung-ver2/components/Calendar.js
@@ -1,11 +1,116 @@
-import React from 'react';
+import {cls} from "../libs/utils"
+import moment from "moment";
+import 'moment/locale/ko';
+import React, { useEffect, useState } from "react";
 
-const Calendar = () => {
-    return (
-        <div>
-            
+
+const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+
+export default function CalendarBar() {
+  const [date, setDate] = useState(moment())
+  const [yearClicked, setYearClicked] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(Number(date.clone().format("YYYY")))
+  const [today, setToday] = useState(date.clone().format("YYYY-MM-DD"))
+
+  const handleMonth = (num) => (num ? setDate(date.clone().add(1, 'month')) : setDate(date.clone().subtract(1, 'month')));
+  const returnToday  = () => setDate(moment());
+
+  const buildCalendar = () => {
+    const dateStartWeek = date.clone().startOf('month').week(); 
+    // date가 속하는 달의 첫번쨰 주가 이번년도의 몇번째 주인가
+
+    const dateEndWeek = date.clone().endOf('month').week() === 1 ? 53 : date.clone().endOf('month').week();
+     // date가 속하는 달의 마지막 주가 이번년도의 몇번째 주인가
+     // 마지막 주가 1이 나오는 경우(12월의 마지막주에서 1월이 과반수일때)엔 53으로 수정해주어야 함 
+
+     let calendar = [];
+
+     for (let week = dateStartWeek; week <= dateEndWeek; week++) {
+      calendar.push(
+        <div key={week} className="grid grid-cols-7 items-center justify-items-center gap-10 ">
+        {[Array(7).fill(0).map( (n,i) => {
+          let current = date.clone().week(week).startOf('week').add(i, 'day');
+
+          const isMonth = current.format('MM') !== date.format('MM');
+          const isToday = current.clone().format(`YYYY-MM-DD`) === today;
+          
+
+          return <span key={i} className={cls("text-center hover:font-extrabold text-lg px-1", isMonth ? "text-gray-200": "", !isMonth&& isToday ? " bg-[#bed0d9] rounded-full" :"")} >{current.format("D")}</span>
+        })]}  
         </div>
-    );
-};
+        
+      )
+     }
+     return calendar
+  }
+  
+  
+  const onClickYear = () => {
+    setYearClicked(prev => !prev)
+  }
+  
+  const buildYear = () => {
+    let years = [];
 
-export default Calendar;
+
+    const onClickSelectedYear = (i) => {
+      setDate(date.clone().year(i))
+      setYearClicked(false);
+    }
+
+    for (let i = selectedYear-2; i < selectedYear + 4 ; i++) {
+      years.push(<span className={cls(i==date.clone().format('YYYY') ? "text-textPoint": "")} onClick={() =>onClickSelectedYear(i)}>{i}</span>)
+    }
+    
+    return years
+  }
+
+
+  const handleSelectedYear = (num) => num ? setSelectedYear(selectedYear + 12): setSelectedYear(selectedYear - 12);
+  
+  return (
+    <div className="select-none">
+    {yearClicked ?<div className="fixed top-0 bottom-0 z-10 w-screen h-srceen opacity-0" onClick={onClickYear}></div> : null}
+    <div className="">
+      <div className="relative px-5 pb-5 pt-2 text-sm m-2 ">
+        <div className=" flex items-center flex-row space-x-1 ">
+          <span className="text-gray-600 text-lg">{date.clone().format('YYYY')}</span>
+          <span className="cursor-pointer" onClick={onClickYear}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+          </span>
+        </div>
+        <div className="flex justify-center items-center space-x-1  pb-5 ">
+          <span onClick={()=>handleMonth(0)} className="cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </span>
+          <span className="w-20 text-center font-semibold text-xl" >{date.clone().format("MMMM")}</span>
+          <span onClick={()=>handleMonth(1)} className="cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </span>
+        </div>
+        <div className="grid grid-cols-7 gap-10 justify-items-center items-center pt-3 pb-10">
+          {dayOfWeek.map((d,i) => (
+              <span key={i} className="text-sm font-bold">{d}</span>
+          ))}
+          </div>
+        <div className="space-y-5 sm:space-y-10" >{buildCalendar()}</div>
+        {yearClicked ? 
+        <div className="absolute shadow-2xl top-8 p-3 z-50 rounded-md bg-white">
+          <div className=" grid">
+            {buildYear()}
+          </div>
+        </div>
+        : null}
+
+      </div>
+    </div>  
+    </div>
+    
+  );
+};
--- daechung-ver2/pages/index.js
+++ daechung-ver2/pages/index.js
@@ -4,32 +4,55 @@ import { Inter } from '@next/font/google'
 import Bar from '@/components/Bar'
 import tw from 'tailwind-styled-components'
 import Calendar from '@/components/Calendar'
+import profile from "../public/대충이 프로필 사진.png"
+import pin from "../public/핀.png"
 
 const Profile = tw.div`
-
+  flex
+  items-center
+  my-4
+  space-x-2
+  cursor-pointer
+  ml-16
 `
-
+const CateItem = tw.div`
+  bg-[#F0F0F0]
+  py-2
+  px-6
+  rounded-bl-2xl
+  rounded-se-2xl 
+`
 export default function Home() {
   return(
-    <div>
+    <div className='w-screen flex flex-col h-screen'>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"></link>
          {/* font-light:300 font-normal:400 font-bold:700 */}
       </Head>
-      <div className='md:w-[600px] mx-auto h-screen border-l border-r'>
-        <Profile className=''>
-          <Image/>
+      <div className='w-full sm:w-[640px] flex-grow  mx-auto  border-l border-r px-5 py-5 sm:py-10 '>
+        <Profile>
+          <Image src={profile} className='w-16 rounded-full border-[1px]'/>
           <span>대충이</span>
         </Profile>
-        <div>
-          <img/> {/* 캘린더 집게 이미지 */}
-          <span className='bg-mainColor p-1 px-4 rounded-xl'>Calendar</span>
+        <div className='relative flex flex-col items-center mt-3 mb-5 px-16'>
+          <Image src = {pin}  className='w-5 absolute -top-3 rotate-[30deg]'/> 
+          <span className='bg-mainColor p-1 px-6 rounded-xl'>Calendar</span>
           <div>
             <Calendar/>
           </div>
-
+        </div>
+        <div className='space-y-2 mb-12'>
+          <CateItem>
+            국민은행 서포터즈
+          </CateItem>
+          <CateItem>
+            브랜드 전략
+          </CateItem>
+          <CateItem>
+            디지털전환과 비즈니스 모델링
+          </CateItem>
         </div>
       </div>
       <Bar/>
