068bbf03479013366a851693683c5f1ef1fe7650
juhui88 <sand8594@hufs.ac.kr>
2023-06-01 09:22:13 +0900

    Fix 캘린더 padding

--- daechung-ver2/components/Layout.js
+++ daechung-ver2/components/Layout.js
@@ -1,27 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Bar from './Bar';
-import { useRouter } from 'next/router';
-import { isMobile } from 'react-device-detect';
+import React, { useEffect, useState } from "react";
+import Bar from "./Bar";
+import { useRouter } from "next/router";
+import { isMobile } from "react-device-detect";
+import { cls } from "@/libs/utils";
 
-const Layout = ({children}) => {
-    const router = useRouter()
-    const [width, setWidth] = useState(false)
+const Layout = ({ children }) => {
+  const router = useRouter();
+  const [width, setWidth] = useState(false);
+  console.log(router.pathname);
+  return (
+    <div className="w-screen flex flex-col h-screen">
+      <div
+        className={cls(
+          " sm:w-[640px] flex-grow mx-auto  border-l border-r mb-[52px] ",
+          router.pathname === "/" ? "w-screen" : "w-full"
+        )}
+      >
+        {children}
+      </div>
 
-
-    return (
-        <div className='w-screen flex flex-col h-screen'>
-            <div className='w-full sm:w-[640px] flex-grow mx-auto  border-l border-r mb-[52px] '>
-                {children}
-            </div>
-            
-            <div>
-                {isMobile ? router.pathname === "/main" || router.pathname === "/notes" || router.pathname === "/porfolio" ? <Bar/> :null : router.pathname==="/login" ? null: <Bar/> }
-            </div>   
-
-            
-            
-        </div>
-    );
+      <div>
+        {isMobile ? (
+          router.pathname === "/main" ||
+          router.pathname === "/notes" ||
+          router.pathname === "/porfolio" ? (
+            <Bar />
+          ) : null
+        ) : router.pathname === "/login" || router.pathname === "/" ? null : (
+          <Bar />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
--- daechung-ver2/pages/index.js
+++ daechung-ver2/pages/index.js
@@ -1,23 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import Bar from '@/components/Bar'
-import tw from 'tailwind-styled-components'
-import Calendar from '@/components/Calendar'
-import profile from "../public/대충이 프로필 사진.png"
-import pin from "../public/핀.png"
-
-
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  return(
-    <div className='w-screen flex flex-col h-screen'>
+  return (
+    <div className="w-screen flex flex-col h-screen">
       <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>    
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
+      <Layout>njk</Layout>
     </div>
-
-  )
+  );
 }
